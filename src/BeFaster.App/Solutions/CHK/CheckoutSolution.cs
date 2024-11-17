42678c0754af44d34d6549042e7e78eac7e3ef50
sbx_user1051 <sbx_user1051@169.254.53.189>
2024-11-17 18:32:41 +0000

    Sun Nov 17 18:32:41 UTC 2024

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -6,7 +6,37 @@ namespace BeFaster.App.Solutions.CHK
     {
         public static int ComputePrice(string? skus)
         {
-            throw new SolutionNotImplementedException();
+            if(string.IsNullOrEmpty(skus))
+                return -1;
+            
+
+
+            return ComputeIndividualPrice();
+        }
+
+        private static int ComputeIndividualPrice(string sku, int numberOfItems = 1)
+        {
+            switch (sku)
+            {
+                case "A":
+                    return CalculatePriceIncludingDiscount(numberOfItems, 50, 130, 3);
+                case "B":
+                    return CalculatePriceIncludingDiscount(numberOfItems, 30, 45, 2);
+                case "C":
+                    return 20 * numberOfItems;
+                case "D":
+                    return 15 * numberOfItems;
+                default:
+                    return -1;
+            }
+        }
+
+        private static int CalculatePriceIncludingDiscount(int numberOfItems, int individualPrice, int discountPrice, int numberOfItemsForDiscount)
+        {
+            int discountItemsTotalPrice = numberOfItems / 3 * discountPrice;
+            int individualTotalPrice = numberOfItems % 3 * individualPrice;
+            return discountItemsTotalPrice + individualTotalPrice;
         }
     }
 }
+
